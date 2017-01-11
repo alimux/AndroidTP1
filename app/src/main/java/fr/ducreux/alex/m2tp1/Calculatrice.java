12bb1dc47fee7eff914ae404254e0fcbf5a33335
alimux <alexducreux14@gmail.com>
2017-01-11 10:18:43 +0100

    ajout de la gestion des evenements et de l'opération d'ajout

--- app/src/main/java/fr/ducreux/alex/m2tp1/Calculatrice.java
+++ app/src/main/java/fr/ducreux/alex/m2tp1/Calculatrice.java
@@ -2,6 +2,7 @@ package fr.ducreux.alex.m2tp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,36 +17,41 @@ public class Calculatrice extends AppCompatActivity {
         setContentView(R.layout.activity_calculatrice);
     }
 
-    EditText editText = (EditText) findViewById(R.id.nombreA);
-    editText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
-        @override
-                public
-    })
-
-
-
-
-    public int addOperation(int a, int b)
-    {
-        return  a + b;
+    public void ajouter(View view) {
+        EditText nombreA = (EditText) findViewById(R.id.nombreA);
+        int a =  Integer.valueOf(nombreA.getText().toString());
+        System.out.println("nom a : "+a);
+        EditText nombreB =(EditText) findViewById(R.id.nombreB);
+        int b = Integer.valueOf(nombreB.getText().toString());
+        TextView displayResultat = (TextView) findViewById(R.id.displayResult);
+        char op = '+';
+        int result = operation(a, b, op);
+        displayResultat.setText(Integer.toString(result));
     }
-    public int subtractOperation(int a, int b)
-    {
-        return a - b;
-    }
-    public int multiplyOperation(int a, int b)
-    {
-        return  a * b;
-    }
-    public  int dividedOperation(int a, int b)
-    {
 
-        try {
-            return a / b;
+
+
+    public int operation(int a, int b, char op){
+        int r = 0;
+        switch (op){
+            case '+' : r =  a + b;
+            break;
+            case '-' : r = a - b;
+            break;
+            case '*' : r = a * b;
+            break;
+            case '/' :
+                try {
+                r = a / b;
+                }
+                catch (ArithmeticException e){
+                //affichage message d'erreur...
+                return 0;
+                }
+                break;
         }
-        catch (ArithmeticException e){
-            //affichage message d'erreur...
-            return 0;
-        }
+        return r;
+
     }
+
 }
